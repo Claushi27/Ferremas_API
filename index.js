ea4e11ece3fdf186b64f43e499a9e7f1bf42679e
Ernesto Velasquez <ernestoleonidas@gmail.com>
2025-04-23 17:55:36 -0400

    update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// index.js
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
 const authRoutes = require('./routes/authRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
 
 app.use(express.json());
 app.use('/login', authRoutes);
+app.use('/usuarios', usuarioRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
